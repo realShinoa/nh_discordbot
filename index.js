09640734996aee2b5a991e15a958090ad111e24b
realShinoa <72938300+realShinoa@users.noreply.github.com>
2020-10-26 04:38:19 +0800

    x

--- index.js
+++ index.js
@@ -6,146 +6,150 @@ const fs = require('fs');
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
+for (const file of commandFiles) {
+   const command = require(`./commands/${file}`);
 
-    client.commands.set(command.name, command);
+   client.commands.set(command.name, command);
 }
 
 client.once('ready', () => {
    console.log('Bot is online');
-   client.user.setActivity('Nocturnal Hub, Use .help', { type : 'WATCHING'}).catch(console.error)
+   client.user.setActivity('Nocturnal Hub, Use .help', { type: 'WATCHING' }).catch(console.error)
 });
 
-client.on('guildMemberAdd', member =>{
+client.on('guildMemberAdd', member => {
    const channel = member.guild.channels.cache.find(channel => channel.name === '﹕general-chat');
-   if(!channel) return;
+   if (!channel) return;
 
    channel.send(`${member}`)
 });
 
-client.on('guildMemberAdd', member =>{
+client.on('guildMemberAdd', member => {
    const channel = member.guild.channels.cache.find(channel => channel.name === '﹕general-chat');
-   if(!channel) return;
+   if (!channel) return;
 
    const embed = new Discord.MessageEmbed()
 
- embed.setTitle("Welcome to Nocturnal Hub!")
- embed.setThumbnail(member.user.displayAvatarURL())
- embed.setDescription(`> We are pleased to have you here, ${member}!  <a:011_nhub:762401995188469810>\n> Read the server information in <#746767768547754095>\n> You can get self roles in <#757120102566199337>\n\n> Welcome to our community and we hope you enjoy your stay here!\n\n\n`)
- embed.setImage('https://cdn.discordapp.com/attachments/765662435436134402/769013221344280596/NH.gif')
- embed.setColor('#663890')
- embed.addField('Total Members', member.guild.memberCount, true)
- embed.setFooter("Nocturnal Hub")
+   embed.setTitle("Welcome to Nocturnal Hub!")
+   embed.setThumbnail(member.user.displayAvatarURL())
+   embed.setDescription(`> We are pleased to have you here, ${member}!  <a:011_nhub:762401995188469810>\n> Read the server information in <#746767768547754095>\n> You can get self roles in <#757120102566199337>\n\n> Welcome to our community and we hope you enjoy your stay here!\n\n\n`)
+   embed.setImage('https://cdn.discordapp.com/attachments/765662435436134402/769013221344280596/NH.gif')
+   embed.setColor('#663890')
+   embed.addField('Total Members', member.guild.memberCount, true)
+   embed.setFooter("Nocturnal Hub")
 
    channel.send(embed)
 });
 
-client.on('message', async message =>{
-   if(!message.content.startsWith(prefix) || message.author.bot) return;
+client.on('message', async message => {
+   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
    const embed = new Discord.MessageEmbed()
    const args = message.content.slice(prefix.length).split(/ +/);
    const command = args.shift().toLowerCase();
+   if (message.content.startsWith(prefix + "ping")) {
+      var ping = Date.now() - message.createdTimestamp + " ms";
+      message.channel.send("Your ping is `" + `${ping}` + " `");
+   }
 
-   if(command === 'avatar'){
+   if (command === 'avatar') {
       client.commands.get('avatar').execute(message, args);
    }
-   if(command === 'help'){
+   if (command === 'help') {
       client.commands.get('help').execute(message, args);
-   }  
+   }
 
-if(command === 'welcome'){
+   if (command === 'welcome') {
       client.commands.get('welcome').execute(message, args);
-  }
-  
-if(command === 'gm'){
-   client.commands.get('gm').execute(message, args);
-}
+   }
 
-if(command === 'slap'){
-   client.commands.get('slap').execute(message, args);
-}
+   if (command === 'gm') {
+      client.commands.get('gm').execute(message, args);
+   }
 
-if(command === 'cry'){
-   client.commands.get('cry').execute(message, args);
-}
+   if (command === 'slap') {
+      client.commands.get('slap').execute(message, args);
+   }
 
-if(command === 'hug'){
-   client.commands.get('hug').execute(message, args);
-}
+   if (command === 'cry') {
+      client.commands.get('cry').execute(message, args);
+   }
 
-if(command === 'kill'){
-   client.commands.get('kill').execute(message, args);
-}
+   if (command === 'hug') {
+      client.commands.get('hug').execute(message, args);
+   }
 
-if(command === 'blush'){
-   client.commands.get('blush').execute(message, args);
-}
+   if (command === 'kill') {
+      client.commands.get('kill').execute(message, args);
+   }
 
-if(command === 'pout'){
-   client.commands.get('pout').execute(message, args);
-}
+   if (command === 'blush') {
+      client.commands.get('blush').execute(message, args);
+   }
 
-if(command === 'smile'){
-   client.commands.get('smile').execute(message, args);
-}
- 
-if(command === 'kiss'){
-   client.commands.get('kiss').execute(message, args);
-}
- 
-if(command === 'pat'){
-   client.commands.get('pat').execute(message, args);
-}
- 
-if(command === 'smug'){
-   client.commands.get('smug').execute(message, args);
-}
- 
-if(command === 'shrug'){
-   client.commands.get('shrug').execute(message, args);
-}
- 
-if(command === 'cuddle'){
-   client.commands.get('cuddle').execute(message, args);
-}
- 
-if(command === 'wave'){
-   client.commands.get('wave').execute(message, args);
-}
+   if (command === 'pout') {
+      client.commands.get('pout').execute(message, args);
+   }
 
-if(command === 'sleepy'){
-   client.commands.get('sleepy').execute(message, args);
-}
+   if (command === 'smile') {
+      client.commands.get('smile').execute(message, args);
+   }
 
-if(command === 'tired'){
-   client.commands.get('tired').execute(message, args);
-}
+   if (command === 'kiss') {
+      client.commands.get('kiss').execute(message, args);
+   }
 
-if(command === 'lick'){
-   client.commands.get('lick').execute(message, args);
-}
+   if (command === 'pat') {
+      client.commands.get('pat').execute(message, args);
+   }
 
-if(command === 'punch'){
-   client.commands.get('punch').execute(message, args);
-}
+   if (command === 'smug') {
+      client.commands.get('smug').execute(message, args);
+   }
 
-if(command === 'gn'){
-   client.commands.get('gn').execute(message, args);
-}
+   if (command === 'shrug') {
+      client.commands.get('shrug').execute(message, args);
+   }
 
-if(command === 'poke'){
-   client.commands.get('poke').execute(message, args);
-}
+   if (command === 'cuddle') {
+      client.commands.get('cuddle').execute(message, args);
+   }
 
-if(command === 'bite'){
-   client.commands.get('bite').execute (message, args);
-}
+   if (command === 'wave') {
+      client.commands.get('wave').execute(message, args);
+   }
 
-if(command === 'fu'){
-   client.commands.get('fu').execute(message, args);
-}
+   if (command === 'sleepy') {
+      client.commands.get('sleepy').execute(message, args);
+   }
+
+   if (command === 'tired') {
+      client.commands.get('tired').execute(message, args);
+   }
+
+   if (command === 'lick') {
+      client.commands.get('lick').execute(message, args);
+   }
+
+   if (command === 'punch') {
+      client.commands.get('punch').execute(message, args);
+   }
+
+   if (command === 'gn') {
+      client.commands.get('gn').execute(message, args);
+   }
+
+   if (command === 'poke') {
+      client.commands.get('poke').execute(message, args);
+   }
+
+   if (command === 'bite') {
+      client.commands.get('bite').execute(message, args);
+   }
+
+   if (command === 'fu') {
+      client.commands.get('fu').execute(message, args);
+   }
 
 });
 client.login(process.env.token);

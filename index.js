5b20ea0f8f229a66d260de8d3b345b50548b7187
realShinoa <72938300+realShinoa@users.noreply.github.com>
2020-11-06 16:36:20 +0800

    xa

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ client.once('ready', () => {
 });
 
 client.on('guildMemberAdd', member => {
-   const channel = member.guild.channels.cache.find(ch => ch.name === 'general-chat');
+   const channel = member.guild.channels.cache.find(ch => ch.name === ': general-chat');
    if (!channel) return;
    channel.send(`Welcome to the server, ${member}`);
 });
